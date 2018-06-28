96e30b1d5b1cc0fe774c8fa5dabfed34e1bffc70
Yuji Suzuki <y0suzuki0123@gmail.com>
2018-06-28 17:11:15 +0900

    Create helper function of getComponent

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,16 @@
 import { renderReact } from 'hypernova-react';
 import * as hypernova from 'hypernova/server';
+import * as path from 'path';
+
+const createGetComponent = (files: any, vmOptions?: any) => {
+  const getComponent = hypernova.createGetComponent(files, vmOptions);
+  return (name: string) => renderReact(name, getComponent(name)[name]);
+};
 
 hypernova({
-  getComponent(name) {
-    if (name === 'App') {
-      const { App } = require('../dist/components/App.js');
-      return renderReact('App', App);
-    }
-    return null;
-  },
+  getComponent: createGetComponent({
+    App: path.resolve(path.join('dist', 'components', 'App.js'))
+  }),
   devMode: true,
   host: 'localhost',
   port: 4000
